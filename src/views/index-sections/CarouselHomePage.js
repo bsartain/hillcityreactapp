7ab20bbc5393d678843343d4fed04004afff9eeb
Brett Sartain <brettsartain@Bretts-MBP.lan>
2021-03-22 12:59:13 -0400

    Fixed length of slides

--- src/views/index-sections/CarouselHomePage.js
+++ src/views/index-sections/CarouselHomePage.js
@@ -2,47 +2,19 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Carousel, CarouselItem } from "reactstrap";
 
-const items = [
-  {
-    id: 1,
-    altText: "Slide 1",
-    caption: "Slide 1",
-  },
-  {
-    id: 2,
-    altText: "Slide 2",
-    caption: "Slide 2",
-  },
-  {
-    id: 3,
-    altText: "Slide 3",
-    caption: "Slide 3",
-  },
-  {
-    id: 4,
-    altText: "Slide 4",
-    caption: "Slide 4",
-  },
-  {
-    id: 5,
-    altText: "Slide 5",
-    caption: "Slide 5",
-  },
-];
-
 const CarouselHomePage = ({ headerData }) => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [animating, setAnimating] = useState(false);
 
   const next = () => {
     if (animating) return;
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    const nextIndex = activeIndex === headerData.homepage_slides.length - 1 ? 0 : activeIndex + 1;
     setActiveIndex(nextIndex);
   };
 
   const previous = () => {
     if (animating) return;
-    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    const nextIndex = activeIndex === 0 ? headerData.homepage_slides.length - 1 : activeIndex - 1;
     setActiveIndex(nextIndex);
   };
 
