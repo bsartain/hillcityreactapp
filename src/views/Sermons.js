ce382ac62c84d999e7279a74d517f5fe0d7e3088
Brett Sartain <brettsartain@Bretts-MBP.lan>
2021-12-09 17:22:03 -0500

    Set the pagination to scroll to the filter. Added a blurb ablout podcast info.

--- src/views/Sermons.js
+++ src/views/Sermons.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from 'react';
 
-import PageHeader from "components/Headers/PageHeader.js";
-import SpinnerFullPage from "components/Spinner/SpinnerFullPage";
-import { SermonContent } from "views/SermonContent";
-import { Pagination } from "views/Pagination";
-import SermonFilter from "views/index-sections/SermonFilter";
+import PageHeader from 'components/Headers/PageHeader.js';
+import SpinnerFullPage from 'components/Spinner/SpinnerFullPage';
+import { SermonContent } from 'views/SermonContent';
+import { Pagination } from 'views/Pagination';
+import SermonFilter from 'views/index-sections/SermonFilter';
 
-import { useObserver } from "mobx-react";
-import { StoreContext } from "stores/StoreContext";
-import { useHistory } from "react-router-dom";
-import { googleAnalyticsTrackPage } from "utils/utils";
-import Meta from "components/Meta";
-import { Spinner } from "reactstrap";
+import { useObserver } from 'mobx-react';
+import { StoreContext } from 'stores/StoreContext';
+import { useHistory } from 'react-router-dom';
+import { googleAnalyticsTrackPage } from 'utils/utils';
+import Meta from 'components/Meta';
+import { Spinner } from 'reactstrap';
 
 function Sermons() {
   const store = useContext(StoreContext);
@@ -24,14 +24,14 @@ function Sermons() {
   useEffect(() => {
     googleAnalyticsTrackPage(history.location.pathname);
 
-    document.body.classList.add("index-page");
-    document.body.classList.add("sidebar-collapse");
-    document.documentElement.classList.remove("nav-open");
+    document.body.classList.add('index-page');
+    document.body.classList.add('sidebar-collapse');
+    document.documentElement.classList.remove('nav-open');
     window.scrollTo(0, 0);
     document.body.scrollTop = 0;
     return function cleanup() {
-      document.body.classList.remove("index-page");
-      document.body.classList.remove("sidebar-collapse");
+      document.body.classList.remove('index-page');
+      document.body.classList.remove('sidebar-collapse');
     };
   }, [store, history.location.pathname]);
 
@@ -63,11 +63,28 @@ function Sermons() {
             ) : (
               <div className="page-content-title">
                 <Meta
-                  title={"Sermons | Hill City Church | Rock Hill SC"}
-                  description={"Hill City Church delivers relevent sermons directly from the scriptures to highlight what God has done for us through Jesus Christ"}
+                  title={'Sermons | Hill City Church | Rock Hill SC'}
+                  description={'Hill City Church delivers relevent sermons directly from the scriptures to highlight what God has done for us through Jesus Christ'}
                   image={store.sermonStoreTwo.sermonData[0].featured_image.large}
                   url={window.location.href}
                 />
+                <div className="sermon-podcast-blurb">
+                  All sermon audio is available for free on iTunes or your favorite podcast app.
+                  <br /> Simply subscribe to the Hill City Church Audio podcast.
+                  <div>
+                    <div className="podcast-icons">
+                      <a href="https://podcasts.apple.com/us/podcast/hill-city-church-rock-hill-sc/id1529110625" target="_blank">
+                        <i className="fab fa-itunes"></i>
+                      </a>
+                      <a href="https://open.spotify.com/show/689D8k7FZnLQe1KZTJKEZh?si=73dccf8187964e6c" target="_blank">
+                        <i className="fab fa-spotify"></i>
+                      </a>
+                      <a href="https://podcasts.apple.com/us/podcast/hill-city-church-rock-hill-sc/id1529110625" target="_blank">
+                        <i className="fas fa-podcast"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
                 <h2 className="container">Current Sermons</h2>
                 <hr className="page-content-hr" />
                 <SermonFilter />
